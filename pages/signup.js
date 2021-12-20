03f03362a5bd10d3a12d345e029b4abbc1fe536a
ArefNasser <92294188+ArefNasser@users.noreply.github.com>
2021-12-20 14:05:16 +0300

    signUp added

--- pages/signup.js
+++ pages/signup.js
@@ -1,9 +1,23 @@
 const signup = () => {
   return (
     <div>
-      
+      <div>
+        <div>
+          <h1>Sign up</h1>
+          <input type="text" name="Name" placeholder="Name" />
+          <input type="text" name="Email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <input
+            type="password"
+            name="confirm_password"
+            placeholder="Confirm Password"
+          />
+          <button type="submit">Create Account</button>
+          <Link href="/">Do you have an account?</Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default signup
+export default signup;
