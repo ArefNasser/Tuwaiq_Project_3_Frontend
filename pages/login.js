19bb52c80abece5e455b1a329abd1fb43900dc35
Houryah <92294225+Houryah@users.noreply.github.com>
2021-12-20 13:45:51 +0300

    login

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,19 @@
+import Link from "next/link";
+
 const login = () => {
   return (
     <div>
-      
+      <div>
+        <div>
+          <h1>Login</h1>
+          <input type="text" name="Email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <button type="submit">Login</button>
+          <Link to="/signup">Create an account</Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default login
+export default login;
