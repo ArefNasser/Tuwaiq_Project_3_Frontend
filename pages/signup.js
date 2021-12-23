b6e14d4cb9cbb2a0bcc20a3becad3ff9ab63272a
ArefNasser <92294188+ArefNasser@users.noreply.github.com>
2021-12-23 09:30:22 +0300

    signUp2 added

--- pages/signup.js
+++ pages/signup.js
@@ -1,18 +1,62 @@
+import Link from "next/link";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+
 const signup = () => {
+  const router = useRouter();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const hundleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const hundleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const hundlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handlePasswordConfirm = (e) => {
+    setPasswordConfirm(e.target.value);
+  };
+
+  const hundleRegister = (e) => {
+    e.preventDefault();
+    if (password === passwordConfirm) {
+      let user = {
+        name: name,
+        email: email,
+        password: password,
+      };
+
+      axios
+        .post("http://localhost:5000/users/add", user)
+        .then((res) => {
+          console.log(res.data);
+          router.push("/");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
   return (
     <div>
       <div>
         <div>
           <h1>Sign up</h1>
-          <input type="text" name="Name" placeholder="Name" />
-          <input type="text" name="Email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
-          <input
+          <input onChange={hundleNameChange} type="text" name="Name" placeholder="Name" />
+          <input onChange={hundleEmailChange} type="text" name="Email" placeholder="Email" />
+          <input onChange={hundlePasswordChange} type="password" name="password" placeholder="Password" />
+          <input onChange={handlePasswordConfirm}
             type="password"
             name="confirm_password"
             placeholder="Confirm Password"
           />
-          <button type="submit">Create Account</button>
+          <button onClick={hundleRegister} type="submit">Create Account</button>
           <Link href="/">Do you have an account?</Link>
         </div>
       </div>
